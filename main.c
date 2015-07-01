f2b56a74dc93e8e8ffeb60ad0772cb416f8237dc
manno23 <jasonmanning23@gmail.com>
2015-07-01 16:35:03 +0800

    first commit

---
+++ main.c
@@ -0,0 +1,53 @@
+/******************************************************************************/
+/* Files to Include                                                           */
+/******************************************************************************/
+
+/* Device header file */
+#if defined(__XC16__)
+    #include <xc.h>
+#elif defined(__C30__)
+    #if defined(__PIC24E__)
+    	#include <p24Exxxx.h>
+    #elif defined (__PIC24F__)||defined (__PIC24FK__)
+	#include <p24Fxxxx.h>
+    #elif defined(__PIC24H__)
+	#include <p24Hxxxx.h>
+    #endif
+#endif
+
+#include <stdint.h>        /* Includes uint16_t definition                    */
+#include <stdbool.h>       /* Includes true/false definition                  */
+
+#include "system.h"        /* System funct/params, like osc/peripheral config */
+#include "user.h"          /* User funct/params, such as InitApp              */
+
+/******************************************************************************/
+/* Global Variable Declaration                                                */
+/******************************************************************************/
+
+uint16_t frigbot_status_flags __attribute__((space(data),address(0x800)));
+
+/******************************************************************************/
+/* Main Program                                                               */
+/******************************************************************************/
+
+int16_t main(void)
+{
+
+    /* Configure the oscillator for the device */
+    ConfigureOscillator();
+
+    /* Initialize IO ports and peripherals */
+    InitApp();
+
+    if (frigbot_status_flags & 0x0001) {
+        /* Read hexfile program description at 0x2C00 and write this over
+           the top of program 1, beginning at C00. */
+        /* Write new program over old. */
+        frigbot_status_flags = 0;       // Clear new firmware flag
+    } else {
+        /* Continue to program location */
+        asm("goto 0xC00");
+    }
+  
+}
